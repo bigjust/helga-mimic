a38ad20cfcaf498be49f2e43b0d2927f5592c6df
Justin Caratzas <bigjust@lambdaphil.es>
2016-11-13 22:21:25 -0500

    increase tries, open to non-operators

--- helga_mimic.py
+++ helga_mimic.py
@@ -28,8 +28,7 @@ def generate_sentence(channel_or_user):
     else:
         db_filter = {'nick': channel_or_user}
 
-    if db.logger.find(db_filter).count() < 1000:
-        return 'not enough material'
+    logger.debug('{} lines found'.format(db.logger.find(db_filter).count()))
 
     corpus = ''
     for doc in db.logger.find(db_filter):
@@ -37,22 +36,19 @@ def generate_sentence(channel_or_user):
         corpus += '\n'
 
     text_model = markovify.NewlineText(corpus)
-    return text_model.make_sentence(tries=3)
+    return text_model.make_sentence(tries=50)
 
 @command('mimic', help='mimics user or channel specified')
 def mimic(client, channel, nick, message, cmd, args):
 
     logger.debug('mimic initiated')
-    logger.debug('channel: {}'.format(channel))
-    logger.debug('nick: {}'.format(nick))
-    logger.debug('OPS: {}'.format(OPS))
-
-    if channel not in OPS or nick not in OPS:
-        logger.debug('not authed, quitting')
-        return
 
     if not args[0]:
         return 'must specify a thing'
 
-    generate_sentence(args[0])
-    return generate_sentence(args[0])
+    generated = generate_sentence(args[0])
+
+    if not generated:
+        return 'i got nothing :/'
+
+    return generated

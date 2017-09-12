da14ff23c4293c1b098aba76f94456e56e04629a
Justin Caratzas <bigjust@lambdaphil.es>
2017-09-12 11:39:03 -0400

    don't assume match.groups() is a thing

--- helga_mimic.py
+++ helga_mimic.py
@@ -199,10 +199,13 @@ class MimicPlugin(Command):
     def preprocess(self, client, channel, nick, message):
         if NICK in message:
             response = bot_say(seed=message)
-            potential_nick = ADDRESSING_POSSIBLE_NICK.match(response).groups()[0]
+            potential_nick_matches = ADDRESSING_POSSIBLE_NICK.match(response).groups()
 
-            if is_alias(potential_nick):
-                response = response.replace(potential_nick, nick)
+            if potential_nick_matches:
+                potential_nick = potential_nick_matches[0]
+
+                if is_alias(potential_nick):
+                    response = response.replace(potential_nick, nick)
 
             client.msg(channel, response)
             self.last_response = response
